6e6972c74c2c0973cc697f4c6f4f3fbd3f104969
Nick <ndenietolis@gmail.com>
2022-08-10 21:22:40 -0400

    working recursive to csv

--- json_to_csv.rb
+++ json_to_csv.rb
@@ -1,11 +1,36 @@
 require 'json'
 
-json_file = ARGV[0]
+def print_data(data, csv, count)
+  data.each do |key, value|
+    csv.write ["," * count, (key if key.class != Hash && key.class != Array), "," , (value if value.class != Hash && value.class != Array), "\n"].join('')
+    begin
+      get_everything(data[key], csv, count + 1)
+    rescue 
+      next
+    end
+  end
+end
 
-json = File.read json_file
+def get_everything(data, csv, count=0)
+  new_count = count
+  case 
+  when data.class == Hash
+    print_data(data, csv, new_count)
+  when data.class == Array
+    data.each do |data|
+      print_data(data, csv, new_count)
+    end
+  end
+end
 
-json_data = JSON.parse json
+if __FILE__ == $PROGRAM_NAME
+  json_file = ARGV[0]
+  json = File.read json_file
+  json_data = JSON.parse json
 
-pp json_data
+  puts "creating new file: #{File.basename(json_file, ".*")}.csv"
 
+  csv = File.open("#{File.basename(json_file, ".*")}.csv", 'w')
+
+  get_everything(json_data, csv)
 end
